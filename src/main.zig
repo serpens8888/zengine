e7ee328aafcbac0e3eaf833c57116f7ab07dfec2
serpens8888 <rustyruns1@gmail.com>
2024-11-30 17:24:11 -0500

    tried to get layers and debug utils messenger, however it seems that the VK_LAYER_KHRONOS_validation is not being added to the creation info with the current setup on my machine

--- src/main.zig
+++ src/main.zig
@@ -14,11 +14,14 @@ pub fn main() !void {
     }
     defer c.SDL_Quit();
 
+    const window = c.SDL_CreateWindow("zengine window", 600, 600, c.SDL_WINDOW_VULKAN);
+    defer c.SDL_DestroyWindow(window);
+
     var sdl_required_extension_count: u32 = undefined;
     const sdl_extensions = c.SDL_Vulkan_GetInstanceExtensions(&sdl_required_extension_count);
     const sdl_extensions_slice = sdl_extensions[0..sdl_required_extension_count];
 
-    _ = try vk_init.create_vulkan_instance(std.heap.page_allocator, .{
+    const instance: vk_init.Instance = try vk_init.create_vulkan_instance(std.heap.page_allocator, .{
         .app_name = "zengine",
         .app_version = .{ .major = 0, .minor = 0, .patch = 0 },
         .engine_name = "zengine",
@@ -26,5 +29,26 @@ pub fn main() !void {
         .api_version = .{ .major = 1, .minor = 3, .patch = 0 },
         .debug = true,
         .required_extensions = sdl_extensions_slice,
+        .alloc_callback = null,
+        .debug_callback = null,
     });
+
+    var event: c.SDL_Event = undefined;
+    var running: bool = true;
+    while (running) {
+        while (c.SDL_PollEvent(&event)) {
+            switch (event.type) {
+                c.SDL_EVENT_QUIT => {
+                    running = false;
+                    break;
+                },
+                else => {},
+            }
+        }
+
+        // simulate
+    }
+    // USING DEFER WILL SEGFAULT THEY MUST BE IN THIS ORDER
+    vk_init.destroy_debug_utils_messenger(instance, null);
+    vk_init.destroy_instance(instance, null);
 }
--- src/vulkan_init.zig
+++ src/vulkan_init.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 const c = @import("clibs.zig");
+const check = @import("check_vk.zig");
 
 const log = std.log.scoped(.vulkan_init);
 
@@ -10,7 +11,7 @@ const version = struct {
     patch: u32,
 };
 
-pub const vk_info = struct {
+pub const vk_instance_info = struct {
     app_name: [:0]const u8 = "zengine",
     app_version: version,
     engine_name: [:0]const u8 = "zengine",
@@ -18,10 +19,13 @@ pub const vk_info = struct {
     api_version: version,
     debug: bool,
     required_extensions: []const [*c]const u8,
+    debug_callback: c.PFN_vkDebugUtilsMessengerCallbackEXT,
+    alloc_callback: ?*c.VkAllocationCallbacks,
 };
 
-pub const instance = struct {
-    handle: c.VkInstance,
+pub const Instance = struct {
+    handle: c.VkInstance = null,
+    debug_messenger: c.VkDebugUtilsMessengerEXT = null,
 };
 
 fn find_extension(ext_name: [*c]const u8, ext_props: []c.VkExtensionProperties) bool {
@@ -36,7 +40,7 @@ fn find_extension(ext_name: [*c]const u8, ext_props: []c.VkExtensionProperties) 
 
 fn find_layer(layer_name: [*c]const u8, layer_props: []c.VkLayerProperties) bool {
     for (layer_props) |prop| {
-        const prop_name: [*c]const u8 = @ptrCast(prop.extensionName[0..]);
+        const prop_name: [*c]const u8 = @ptrCast(prop.layerName[0..]);
         if (std.mem.eql(u8, std.mem.span(layer_name), std.mem.span(prop_name))) {
             return true;
         }
@@ -44,7 +48,7 @@ fn find_layer(layer_name: [*c]const u8, layer_props: []c.VkLayerProperties) bool
     return false;
 }
 
-pub fn create_vulkan_instance(allocator: std.mem.Allocator, app_info: vk_info) !instance {
+pub fn create_vulkan_instance(allocator: std.mem.Allocator, app_info: vk_instance_info) !Instance {
     var vulkan_version: u32 = c.VK_MAKE_VERSION(app_info.api_version.major, app_info.api_version.minor, app_info.api_version.minor);
 
     if (c.vkEnumerateInstanceVersion(&vulkan_version) != c.VK_SUCCESS) {
@@ -57,13 +61,11 @@ pub fn create_vulkan_instance(allocator: std.mem.Allocator, app_info: vk_info) !
 
     log.info(" Vulkan version: {}.{}.{}", .{ major, minor, patch });
 
-    //var enable_validation = app_info.debug;
+    const enable_validation = app_info.debug;
 
     var arena_state = std.heap.ArenaAllocator.init(allocator);
     defer arena_state.deinit();
     const arena = arena_state.allocator();
-
-    const a: instance = std.mem.zeroInit(instance, .{null});
 
     var app_ci: c.VkApplicationInfo = std.mem.zeroInit(c.VkApplicationInfo, .{0});
 
@@ -83,8 +85,6 @@ pub fn create_vulkan_instance(allocator: std.mem.Allocator, app_info: vk_info) !
         log.err("failed to get extension count", .{});
     }
 
-    log.info(" Vulkan extension count : {}", .{extension_count});
-
     const extension_props: []c.VkExtensionProperties = try arena.alloc(c.VkExtensionProperties, extension_count);
 
     if (c.vkEnumerateInstanceExtensionProperties(null, &extension_count, extension_props.ptr) != c.VK_SUCCESS) {
@@ -102,8 +102,22 @@ pub fn create_vulkan_instance(allocator: std.mem.Allocator, app_info: vk_info) !
     }
 
     var layer_count: u32 = 0;
-    if (c.vkEnumerateInstanceLayerProperties(&layer_count, null) != c.VK_SUCCESS) {
-        log.err("failed to get layer count", .{});
+    var layers = std.ArrayListUnmanaged([*c]const u8){};
+    if (enable_validation == true) {
+        if (c.vkEnumerateInstanceLayerProperties(&layer_count, null) != c.VK_SUCCESS) {
+            log.err("failed to get layer count", .{});
+        }
+
+        const layer_props: []c.VkLayerProperties = try arena.alloc(c.VkLayerProperties, layer_count);
+        if (c.vkEnumerateInstanceLayerProperties(&extension_count, layer_props.ptr) != c.VK_SUCCESS) {
+            log.err("failed to get layer names", .{});
+        }
+
+        const validation_layer: [*c]const u8 = "VK_LAYER_KHRONOS_validation";
+
+        if (find_layer(validation_layer, layer_props)) {
+            try layers.append(arena, validation_layer);
+        }
     }
 
     instance_ci.sType = c.VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
@@ -111,8 +125,71 @@ pub fn create_vulkan_instance(allocator: std.mem.Allocator, app_info: vk_info) !
     instance_ci.pApplicationInfo = &app_ci;
     instance_ci.enabledExtensionCount = @as(u32, @intCast(extensions.items.len));
     instance_ci.ppEnabledExtensionNames = extensions.items.ptr;
-    //    instance_ci.enabledLayerCount =
-    //    instance_ci.ppEnabledLayerNames =
+    instance_ci.enabledLayerCount = @as(u32, @intCast(layers.items.len));
+    instance_ci.ppEnabledLayerNames = layers.items.ptr;
 
-    return a;
+    var instance_handle: c.VkInstance = undefined;
+    if (c.vkCreateInstance(&instance_ci, app_info.alloc_callback, &instance_handle) != c.VK_SUCCESS) {
+        log.err("failed to create vulkan instance", .{});
+    }
+    log.info("created vulkan instance", .{});
+
+    const debug_messenger = if (enable_validation)
+        try create_debug_callback(instance_handle, app_info)
+    else
+        null;
+
+    return .{ .handle = instance_handle, .debug_messenger = debug_messenger };
+}
+
+fn get_vulkan_instance_func(comptime Fn: type, instance: c.VkInstance, name: [*c]const u8) Fn {
+    const get_proc_addr: c.PFN_vkGetInstanceProcAddr = @ptrCast(c.SDL_Vulkan_GetVkGetInstanceProcAddr());
+    if (get_proc_addr) |get_proc_addr_fn| {
+        return @ptrCast(get_proc_addr_fn(instance, name));
+    }
+
+    @panic("SDL_Vulkan_GetVkGetInstanceProcAddr returned null");
+}
+
+fn create_debug_callback(instance: c.VkInstance, instance_info: vk_instance_info) !c.VkDebugUtilsMessengerEXT {
+    const create_fn_opt = get_vulkan_instance_func(c.PFN_vkCreateDebugUtilsMessengerEXT, instance, "vkCreateDebugUtilsMessengerEXT");
+    log.info("got debug utils messenger creation function", .{});
+
+    if (create_fn_opt) |create_fn| {
+        const create_info: c.VkDebugUtilsMessengerCreateInfoEXT = .{
+            .sType = c.VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
+            .messageSeverity = c.VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | c.VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | c.VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT,
+            .messageType = c.VK_DEBUG_UTILS_MESSAGE_TYPE_DEVICE_ADDRESS_BINDING_BIT_EXT | c.VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+                c.VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | c.VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT,
+            .pfnUserCallback = instance_info.debug_callback,
+            .pUserData = null,
+            .flags = 0,
+            .pNext = null,
+        };
+        var debug_messenger: c.VkDebugUtilsMessengerEXT = undefined;
+        if (create_fn(instance, &create_info, instance_info.alloc_callback, &debug_messenger) != c.VK_SUCCESS) {
+            log.err("failed to create debugs utils messenger", .{});
+        }
+        return debug_messenger;
+    }
+    log.err("failed to create debug messenger", .{});
+    return null;
+}
+
+pub fn destroy_debug_utils_messenger(instance: Instance, alloc_cb: ?*c.VkAllocationCallbacks) void {
+    if (instance.debug_messenger != null) {
+        return;
+    }
+
+    const destroy_fn_opt = get_vulkan_instance_func(c.PFN_vkDestroyDebugUtilsMessengerEXT, instance.handle, "vkDestroyDebugUtilsMessengerEXT");
+
+    if (destroy_fn_opt) |destroy_fn| {
+        destroy_fn(instance.handle, instance.debug_messenger, alloc_cb);
+        log.info("destroyed debug utils messenger", .{});
+    }
+}
+
+pub fn destroy_instance(instance: Instance, alloc_cb: ?*c.VkAllocationCallbacks) void {
+    c.vkDestroyInstance(instance.handle, alloc_cb);
+    log.info("destroyed vulkan instance", .{});
 }
